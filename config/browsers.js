b25a3ba589920dac1d2aff5738c7c49688e02f54
Joshua Melville <joshua.melville@northwestern.edu>
2018-05-11 17:27:59 +0100

    update browser definitions for webpack

--- config/browsers.js
+++ config/browsers.js
@@ -2,6 +2,7 @@ const e2c = require('electron-to-chromium');
 
 module.exports = [
   'last 2 Chrome versions',
+  'last 2 ChromeAndroid versions',
   'last 2 iOS major versions',
-  e2c.electronToBrowserList('1.7'),
+  e2c.electronToBrowserList('2.0'),
 ];
